85f45aa8eed5dbfe5121d03107b8e545d0c91e3e
solutionspelichet <solutions.pelichet@gmail.com>
2025-10-14 18:29:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,25 @@
-const CACHE="scan-pwa-v1";
-const ASSETS=["./","./index.html","./style.css","./script.js","./manifest.json"];
+const CACHE = 'scan-pwa-v7';
+const ASSETS = [
+  './',
+  './index.html?v=7',
+  './style.css?v=7',
+  './script.js?v=7',
+  './manifest.json'
+];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-self.addEventListener("activate",e=>{
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).catch(()=>cached))
+  );
 });
